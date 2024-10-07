d9da23127b96b36917147c50a993ad9a671761f6
alexiusacademia <alexius.sayco.academia@gmail.com>
2024-10-07 14:58:11 +0800

    Problem 1.1

---
+++ chapter01/problem1.1/main.c
@@ -0,0 +1,31 @@
+/*
+    Program to calculate the gross salary of 
+    Ramesh. Dearness allowance is 40% or basic salary and 
+    house rent allowance is 20% of basic salary. 
+    Write a program to calculate his gross salary.
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float bs;   // Basic salary
+    float da;   // Dearness allowance
+    float hra;  // House rent allowance
+
+    printf("Enter the basic salary: ");
+    scanf("%f", &bs);
+
+    da = bs * 0.4;
+    hra = bs * 0.2;
+
+    float gs;           // Gross salary
+    gs = bs + da + hra; // Calculate the gross salary
+
+    // Print the outputs
+    printf("Ramesh's basic salary: %f\n", bs);
+    printf("Dearness allowance: %f\n", da);
+    printf("House rent allowance: %f\n", hra);
+    printf("Ramesh's gross salary is %f.\n", gs);
+
+    return 0;
+}
