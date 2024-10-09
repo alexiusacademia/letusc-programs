ead900cc2e0cd73189b3ca50b447bb5e77b326ea
alexiusacademia <alexius.sayco.academia@gmail.com>
2024-10-09 23:51:35 +0800

    Promotion of data type

---
+++ chapter02/promotion.c
@@ -0,0 +1,15 @@
+/*
+    Program that demostracts the number type conversion.
+    This programs tries to promote the type of the given value.
+*/
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    float a;
+    a = 9;
+
+    printf("a now is %f\n", a);
+
+    return 0;
+}
