1aeb3994629d77311ed86484b740681f61de77c6
alexiusacademia <alexius.sayco.academia@gmail.com>
2024-10-12 17:55:01 +0800

    Another way of passing an array

--- chapter13/06_pass_array.c
+++ chapter13/06_pass_array.c
@@ -7,11 +7,15 @@
 #include <stdio.h>
 
 void disp(int *, int);
+void disp2(int ptr[], int);
 
 int main(int argc, char const *argv[])
 {
     int marks[] = {90, 87, 88, 92, 95};
     disp(&marks[0], 5);
+
+    printf("\n");
+    disp2(marks, 5);
 
     return 0;
 }
@@ -21,4 +25,11 @@ void disp(int *ptr, int l) {
         printf("%i ", *ptr);
         ptr++;
     }
+}
+
+void disp2(int ptr[], int n) {
+    for (int i=0; i<n; i++) {
+        printf("%i ", ptr[i]);
+    }
+    printf("\n");
 }
